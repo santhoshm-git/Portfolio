a22257a043727dbf42baf8a8a9cb32f3dbe0dcb9
SANTHOSH M <santhoshmeswaran@gmail.com>
2026-05-21 00:29:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// Smooth fade-up animation
 const cards = document.querySelectorAll(".card");
 
-cards.forEach((card, index) => {
-    card.style.opacity = "0";
-    card.style.transform = "translateY(30px)";
+window.addEventListener("load", () => {
 
-    setTimeout(() => {
-        card.style.transition = "0.6s ease";
-        card.style.opacity = "1";
-        card.style.transform = "translateY(0)";
-    }, index * 150);
+    cards.forEach((card, index) => {
+
+        card.style.opacity = "0";
+        card.style.transform = "translateY(40px)";
+
+        setTimeout(() => {
+
+            card.style.transition = "all 0.7s ease";
+
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+
+        }, index * 200);
+
+    });
+
 });
